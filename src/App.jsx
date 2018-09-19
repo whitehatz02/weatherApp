b9a443c529a542bcdcfc99ec1ea0cf152cc70168
solisbrayan <solisbryan138@gmail.com>
2018-09-19 14:37:57 -0500

    setup boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class App extends React.Component {
+  render() {
+    return <h1>Welcome Fronted Group , This is where your team will be displaying your App </h1>
+  }
+}
+
+export default App
