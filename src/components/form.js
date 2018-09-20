13f958fcc9724583075cfd5535e674da437ae98b
solisbrayan <solisbryan138@gmail.com>
2018-09-20 12:04:34 -0500

    working in form component

--- src/components/form.js
+++ src/components/form.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const Form = props =>(
-      
+
           <form onSubmit= {props.getWeather}>
             <input className= "form" type="text" name="city" placeholder="state" />
 
             <input className="form" type="text" name="country" placeholder="country" />
 
-            <button className="btn">Check Weather</button>
+            <button className="btn">Submit</button>
         </form>
 
     );
