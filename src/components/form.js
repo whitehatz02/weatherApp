19bbcdc1d019221a376f219f55ac863ddaa16568
albertosanchez.hc@gmail.com <albertosanchez.hc@gmail.com>
2018-09-20 22:10:21 -0500

    work on css styling

--- src/components/form.js
+++ src/components/form.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Form = props =>(
-      
-          <form onSubmit= {props.getWeather}>
+
+          <center><form onSubmit= {props.getWeather}>
             <input className= "form" type="text" name="city" placeholder="state"/>
 
             <input className="form" type="text" name="country" placeholder="country" />
 
             <button className="btn">Check Weather</button>
-        </form>
+        </form></center>
 
     );
 
